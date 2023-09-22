36f981a35c2d9d45886a674b8d3a2e9f62f2da14
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-09-22 11:56:04 -0300

    chore(frontend): WIP - start to add routes

---
+++ apps/frontend/src/config/routes.ts
@@ -0,0 +1,13 @@
+export const ROUTES = {
+	home: '/',
+	login: '/login',
+	register: '/register',
+	profile: '/profile',
+	users: '/users',
+	user: '/users/:id',
+	user_reservations: '/users/:id/reservations',
+	user_reservation: '/users/:id/reservations/:reservationId',
+	user_feedbacks: '/users/:id/feedbacks',
+	equipment: '/equipments/:id',
+	equipment_reservation: '/equipments/:id/reservation',
+};
