7168b9a030c366aeb4d575deb82cf51766e1d72f
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-09-23 17:23:29 -0300

    feat: configure tailwind and shadcn

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 
 createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
 );
-
-console.log('Hello from main.tsx');
