96e871772f86912f85b88b374e3fc22a99a21f36
Pietro Bondioli <dev@pietrobondioli.com.br>
2023-11-15 11:27:07 -0300

    build: add port env to vite server config

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,11 +2,22 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'#/fe': path.resolve(__dirname, './src'),
+export default defineConfig(() => {
+	const port = Number(process.env.PORT) || 3000;
+
+	return defineConfig({
+		plugins: [react()],
+		resolve: {
+			alias: {
+				'#/fe': path.resolve(__dirname, './src'),
+			},
 		},
-	},
+		server: {
+			port,
+		},
+		preview: {
+			port,
+			host: true,
+		},
+	});
 });
